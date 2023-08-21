7cf4468dc8c3f786dd6027991ca1ba9427fb0d26
Vallari Agrawal <val.agl002@gmail.com>
2023-08-21 16:18:05 +0530

    echo last commit message
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- gh-actions/signed.sh
+++ gh-actions/signed.sh
@@ -2,6 +2,7 @@
 set -e
 trailer=$(git log -1 --no-merges $commit --pretty='%B' | git interpret-trailers --parse 2>&1)
 echo $trailer
+echo $(git log -1 --no-merges $commit --pretty='%B')
 if [[ $trailer != Signed-off-by:* ]]; then
     printf '%s\n' "Commit is not signed, use git commit --amend --signoff" >&2
     exit 1
