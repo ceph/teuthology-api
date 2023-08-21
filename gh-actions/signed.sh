2a239abb4c9c413dba397a200f35a1f9f4741cb5
Vallari Agrawal <val.agl002@gmail.com>
2023-08-21 16:30:36 +0530

    use -x and print commit and trailer
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- gh-actions/signed.sh
+++ gh-actions/signed.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
-set -e
+set -ex
+commmitmsg=$(git log -1 --no-merges $commit --pretty='%B')
+printf "\n Commit: %s" "$commmitmsg"
 trailer=$(git log -1 --no-merges $commit --pretty='%B' | git interpret-trailers --parse 2>&1)
-echo $trailer
-echo $(git log -1 --no-merges $commit --pretty='%B')
+printf "trailer: %s" "$trailer"
 if [[ $trailer != Signed-off-by:* ]]; then
     printf '%s\n' "Commit is not signed, use git commit --amend --signoff" >&2
     exit 1
