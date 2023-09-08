199ee06fc5810f1cb4073b1cd079dbb15df6ac69
Vallari Agrawal <val.agl002@gmail.com>
2023-09-09 00:42:53 +0530

    Add '/edit' and '/delete' routes
    models/presets: add get_preset_id, update_preset, delete_preset
    		and PresetsDatabaseException
    
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- src/teuthology_api/models/presets.py
+++ src/teuthology_api/models/presets.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer, String, UniqueConstraint
 from sqlalchemy.orm import Session
 from . import Base
-from teuthology_api.schemas.presets import PresetsSchema
+from teuthology_api.schemas.presets import PresetSchema
 
 
 
@@ -16,7 +16,13 @@ class Presets(Base):
     __table_args__ = (UniqueConstraint("username", "name"),)
 
 
-def create_preset(db: Session, preset: PresetsSchema):
+class PresetsDatabaseException(Exception):
+    def __init__(self, message, code):
+        super().__init__(message)
+        self.code = code
+
+
+def create_preset(db: Session, preset: PresetSchema):
     new_preset = Presets(**preset.model_dump())
     db.add(new_preset)
     db.commit()
@@ -24,13 +30,42 @@ def create_preset(db: Session, preset: PresetsSchema):
     return new_preset
 
 
-def get_user_presets(db: Session, username: str):
-    return db.query(Presets).filter(Presets.username == username).all()
+def get_presets_by_username(db: Session, username: str):
+    db_preset = db.query(Presets).filter(Presets.username == username).all()
+    return db_preset
 
 
-def get_preset(db: Session, username: str, preset_name: str):
-    return (
+def get_preset_by_username_name(db: Session, username: str, preset_name: str):
+    db_preset = (
         db.query(Presets)
         .filter(Presets.username == username, Presets.name == preset_name)
         .first()
     )
+    return db_preset
+
+
+def get_preset_id(db: Session, preset_id: int):
+    db_preset = db.query(Presets).filter(Presets.id == preset_id).first()
+    return db_preset
+
+
+def update_preset(db: Session, preset_id: int, update_data):
+    preset_query = db.query(Presets).filter(Presets.id == preset_id)
+    db_preset = preset_query.first()
+    if not db_preset:
+        raise PresetsDatabaseException("Preset does not exist - unable to update.", 404)
+    preset_query.filter(Presets.id == preset_id).update(
+        update_data, synchronize_session=False
+    )
+    db.commit()
+    db.refresh(db_preset)
+    return db_preset
+
+
+def delete_preset(db: Session, preset_id: int):
+    preset_query = db.query(Presets).filter(Presets.id == preset_id)
+    db_preset = preset_query.first()
+    if not db_preset:
+        raise PresetsDatabaseException("Preset does not exist - unable to delete.", 404)
+    preset_query.delete(synchronize_session=False)
+    db.commit()
