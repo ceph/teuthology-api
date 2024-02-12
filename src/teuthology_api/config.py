03f304f8754763d1ad124d82e88934c0674856d2
Vallari Agrawal <val.agl002@gmail.com>
2024-02-12 11:21:33 +0530

    run teuthology-kill cmd instead of teuthology.kill.main
    This commit also removes "--user" from schemas/base.py
    
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- src/teuthology_api/config.py
+++ src/teuthology_api/config.py
@@ -22,6 +22,7 @@ class APISettings(BaseSettings):
 
     session_secret_key: str = ""
     archive_dir: str = ""
+    path_to_teuthology: str = ""
 
     model_config = SettingsConfigDict(
         env_file=".env", env_file_encoding="utf-8", extra="ignore"
