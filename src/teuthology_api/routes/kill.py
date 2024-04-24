46620a748599aa357a85bec7abef5129e829ae86
Vallari Agrawal <val.agl002@gmail.com>
2024-04-24 19:36:33 +0530

    services/kill.py: Allow admins to kill any run
    Signed-off-by: Vallari Agrawal <val.agl002@gmail.com>

--- src/teuthology_api/routes/kill.py
+++ src/teuthology_api/routes/kill.py
@@ -1,6 +1,7 @@
 import logging
 
-from fastapi import APIRouter, Depends, Request
+from fastapi import APIRouter, Depends, Request, HTTPException
+from requests.exceptions import HTTPError
 
 from teuthology_api.services.kill import run
 from teuthology_api.services.helpers import get_token
@@ -15,7 +16,7 @@ router = APIRouter(
 
 
 @router.post("/", status_code=200)
-def create_run(
+async def create_run(
     request: Request,
     args: KillArgs,
     logs: bool = False,
@@ -28,5 +29,16 @@ def create_run(
     or else it will SyntaxError: non-dafault
     argument follows default argument error.
     """
-    args = args.model_dump(by_alias=True, exclude_unset=True)
-    return run(args, logs, access_token, request)
+    try:
+        args = args.model_dump(by_alias=True, exclude_unset=True)
+        return await run(args, logs, access_token, request)
+    except HTTPException:
+        raise
+    except HTTPError as http_err:
+        log.error(http_err)
+        raise HTTPException(
+            status_code=http_err.response.status_code, detail=repr(http_err)
+        ) from http_err
+    except Exception as err:
+        log.error(err)
+        raise HTTPException(status_code=500, detail=repr(err)) from err
